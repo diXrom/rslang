127e96bf4bdf958fd2e9e724a7b7936c28fc9ff4
Tolegen Timur <demigodkz@gmail.com>
2022-09-13 04:04:58 +0600

    feat: add responsive style for component StatisticsTabs

--- src/widgets/StatisticsTabs/model/constants.ts
+++ src/widgets/StatisticsTabs/model/constants.ts
@@ -2,11 +2,13 @@ const layoutSprint = {
   pl: 2,
   borderLeft: '4px solid #0288d1',
   textAlign: 'start',
+  marginY: { xs: 2, md: 0 },
 };
 const layoutAudiocall = {
   pl: 2,
   borderLeft: '4px solid #ed6c02',
   textAlign: 'start',
+  marginY: { xs: 2, md: 0 },
 };
 
 const gameHeader = { fontWeight: 'bold', display: 'flex', alignItems: 'center' };
