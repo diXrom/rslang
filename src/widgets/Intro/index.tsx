5c1ecd967f1b3c2ac42e45a680b5d6df045f3626
Tolegen Timur <demigodkz@gmail.com>
2022-09-12 22:12:46 +0600

    feat: add responsive style for component Intro

--- src/widgets/Intro/index.tsx
+++ src/widgets/Intro/index.tsx
@@ -11,23 +11,37 @@ import learnEnglish from './assets/learn_english.png';
 const Intro = () => {
   return (
     <Stack
-      direction={{ xs: 'column', lg: 'row' }}
-      spacing={4}
-      sx={{ height: '90vh', mb: 4, justifyContent: 'center', alignItems: 'center' }}
+      direction={{ xs: 'column', md: 'row' }}
+      spacing={{ xs: 3, md: 0, lg: 4 }}
+      sx={{
+        height: '90vh',
+        mb: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
       component={motion.section}
       {...motionSettings}
     >
-      <Stack spacing={2} sx={{ maxWidth: '360px', alignItems: 'start' }}>
+      <Stack
+        spacing={{ xs: 1, md: 2 }}
+        sx={{ maxWidth: '360px', alignItems: { xs: 'center', md: 'start' } }}
+      >
         <Typography
           variant="h3"
           component={motion.h3}
           variants={fadeAnimation}
           custom={1}
-          sx={{ fontWeight: 'bold' }}
+          sx={{ fontWeight: 'bold', fontSize: { xs: 35, md: 40, lg: 55 } }}
         >
           RSLang
         </Typography>
-        <Typography variant="body1" component={motion.div} variants={fadeAnimation} custom={2}>
+        <Typography
+          variant="body1"
+          component={motion.div}
+          variants={fadeAnimation}
+          custom={2}
+          sx={{ fontSize: { xs: 15, md: 17, lg: 20 }, textAlign: { xs: 'center', md: 'start' } }}
+        >
           Бесплатный, веселый и эффективный способ выучить язык, скорее же присоединяйтесь к нам.
         </Typography>
         <NavLink to={`/${ROUTE_PATH.BOOK}`} className="navLink">
@@ -42,7 +56,12 @@ const Intro = () => {
           </Button>
         </NavLink>
       </Stack>
-      <Box component={motion.div} variants={fadeAnimation} custom={1}>
+      <Box
+        component={motion.div}
+        variants={fadeAnimation}
+        custom={1}
+        sx={{ maxWidth: { xs: '450px', lg: '100%' } }}
+      >
         <Image src={learnEnglish} alt="img" duration={0} height="100%" width="100%" />
       </Box>
     </Stack>
